8a70da15a421986a9a3f2da0860a7bedff56bca2
rayray <raymoshea@gmail.com>
2015-04-29 19:16:38 +0100

    Added Lecturer and Study Model classes

---
+++ TallaghtTimetable/Models/Lecturer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TallaghtTimetable.Models
+{
+    public enum Module { Programming, Web, Database, Architecture }
+    public class Lecturer
+    {
+
+        public int ID { get; set; }
+
+        public int IconsID { get; set; }
+
+        public int StudysID { get; set; }
+        public string Name { get; set; }
+
+        public Module Modules { get; set; }
+
+        public virtual List<Icon> Icons { get; set; }
+
+        public virtual List<Study> Studys { get; set; }
+
+
+    }
+}
---
+++ TallaghtTimetable/Models/Study.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TallaghtTimetable.Models
+{
+    public class Study
+    {
+
+        public int ID { get; set; }
+
+        public int LecturersID { get; set; }
+
+        public string LecturerName { get; set; }
+
+        // public int IconsID { get; set; }
+
+        public DayOfWeek Day { get; set; }
+
+        [DataType(DataType.Time)]
+        public DateTime Time { get; set; } // Admin creates Timetable database
+
+        // public virtual Icon Icons { get; set; }
+
+        public virtual List<Lecturer> Lecturers { get; set; } // In web api have a get so that user can choose by lecturer
+
+        //public LibraryStudy LibraryStudies { get; set; }
+
+        //public Room229 Room229s { get; set; }
+
+        public bool Availability
+        {
+            get;
+            set;
+        }
+
+        public bool Holiday { get; set; }
+
+        public bool Bookroom { get; set; }
+
+
+    }
+}
