6da98d40747454416002a3046d4089139fc5f0b6
rayray <raymoshea@gmail.com>
2015-04-29 19:19:59 +0100

    Added Db context class

---
+++ TallaghtTimetable/Models/RoomDBContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TallaghtTimetable.Models
+{
+    public class RoomDBContext : DbContext
+    {
+        // manually specify name of connection string in config
+        public RoomDBContext()
+            : base("DefaultConnection")
+        {
+            Database.SetInitializer<RoomDBContext>(new DropCreateDatabaseIfModelChanges<RoomDBContext>());
+            // or CreateDatabaseIfNotExists (default)
+            // or DropCreateDatabaseAlways
+        }
+
+        public DbSet<Lecturer> Lecturers { get; set; }
+        public DbSet<Icon> Icons { get; set; }
+
+        public DbSet<Study> Studys { get; set; }
+
+    }
+}
